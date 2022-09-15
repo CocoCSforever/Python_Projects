715a97d20238538399672f513d112342cfa9c41f
CocoCSforever <yijia.ma18@gmail.com>
2022-09-15 14:42:25 -0700

    added def main()

--- lab02/adder.py
+++ lab02/adder.py
@@ -1,6 +1,8 @@
-num1=int(input("Enter a first value:"))
-num2=int(input("Enter a second value:"))
-sum=num1+num2
+def main():
+    num1=int(input("Enter a first value:"))
+    num2=int(input("Enter a second value:"))
+    sum=num1+num2
 
-print(f"The sum of {num1} + {num2} is {sum}")
+    print(f"The sum of {num1} + {num2} is {sum}")
 # f"The sum of {num1} + {num2} is {num1+num2}"
+main()
